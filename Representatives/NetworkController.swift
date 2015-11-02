53efd0f7acb8cbb01bc48890c88ac87b643100e6
Andrew Porter <haporter@me.com>
2015-11-01 17:48:36 -0700

    finish NetworkController

--- Representatives/NetworkController.swift
+++ Representatives/NetworkController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+class NetworkController {
+    
+    static let baseURLString = "http://whoismyrepresentative.com/getall_reps_bystate.php?state="
+    static let jsonAddOn = "&output=json"
+    
+    //Build url
+    static func searchURL(state: String) -> NSURL {
+        
+        return NSURL(string: NetworkController.baseURLString + state + NetworkController.jsonAddOn)!
+    }
+    
+    //
+    static func dataAtURL(url: NSURL, completion: (resultData: NSData?) -> ()) {
+        //Create network call
+        let session = NSURLSession.sharedSession()
+        
+        let dataTask = session.dataTaskWithURL(url) { (data, _, error) -> Void in
+            //Check to see if call was succesfull
+            guard let data = data else {
+                print("error?.localizedDescriptio")
+                completion(resultData: nil)
+                return
+            }
+            
+            completion(resultData: data)
+        }
+        //After check resume Task
+        dataTask.resume()
+    }
+    
+}
